073526e10bf26317ea4961c6f114bb162ed7765e
xiaofu <1366379923@qq.com>
2019-10-11 14:13:58 +0800

    第一天

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.atguigu.gmall.user.controller;
+
+
+import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 获取所有用户信息
+     * @return
+     */
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+        System.out.println("user .....");
+        List<UmsMember> umsMember = userService.getAllUser();
+        return umsMember;
+    }
+
+    @RequestMapping("getReceiveAddressByMemberId")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+        System.out.println("address......");
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
+        return umsMemberReceiveAddresses;
+    }
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String index(){
+        return "hello user";
+    }
+}
